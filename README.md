8fe472d9812d9687f77a1e69c1ff938a1c0bcc7d
Marvell <marvell@weworkfreelance.com>
2024-04-16 18:59:59 -0400

    included interactive shell with install options

---
+++ README.md
@@ -0,0 +1,46 @@
+### Easy bash script for installing tech stack for creating Laravel microservices with Nuxt user interfaces.
+
+
+### Get Started
+
+To user the scrip you will need to make the script executable running the following command
+
+`chmod +x brew_for_devs.sh`
+
+### What does this install?
+
+    - Node 20
+    - Vue Cli 
+    - Php 8.3
+    - Mysql 8.3
+    - Composer
+    - Google Cloud SQL
+    - Google Cloud SDK 
+
+The script starts by navigating to the home directory using `cd ~`. 
+
+Next, it installs Homebrew, a package manager for macOS, by downloading the installation script from the Homebrew GitHub repository and executing it. After the installation, it adds Homebrew to the shell environment and reloads the shell profile.
+
+The script then uses Homebrew to install several packages: Git, Node.js, PHP, MySQL, and Composer. It specifies versions for Node.js, PHP, and MySQL. 
+
+### Decide what packages you need
+
+Select the packages you want to install using commas to seperate the package numbers `1,2,3`
+
+After the installations, the script adds the paths of the installed packages to the shell profile. This is done so that the shell can find these programs. For example, it adds the path to the PHP binary to the PATH environment variable, which allows the shell to find the PHP executable.
+
+The script then installs the Vue CLI, Google Cloud SDK, and Google SQL Proxy. The Vue CLI is installed globally using npm, the Node.js package manager. The Google Cloud SDK and Google SQL Proxy are installed using Homebrew and curl respectively.
+
+Next, the script installs Nuxt.js and Yarn globally using npm. Nuxt.js is a framework for building Vue.js applications, and Yarn is a package manager for JavaScript.
+
+### Final Steps
+
+The script then generates a new SSH key pair for Github or Bitbucket, adds the SSH key to the ssh-agent, and copies the public key to the clipboard. This is done to allow secure communication with Github or Bitbucket. 
+
+Login to your Git hosting provider to add the key.
+
+Finally, run the following commands
+
+`mysql_secure_installation` to secure the MySQL installation 
+
+`brew services start mysql` to start the MySQL service using .
--- brew_for_devs.sh
+++ brew_for_devs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Navigate to the home directory
 cd ~
 
@@ -6,65 +7,78 @@ cd ~
 echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "Homebrew installed successfully!"
+echo ('eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile)
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Install packages git node php mysql composer using Homebrew
-echo "Installing Git, Node, PHP, MySQL, Composer"
-brew install git node php mysql composer
+# Reload the shell profile
+source ~/.zshrc
 
-# Set the correct path for PHP for default PHP version
-echo 'export PATH="/opt/homebrew/opt/php@8.3/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/opt/homebrew/opt/php@8.3/sbin:$PATH"' >> ~/.zshrc
-echo "PHP installed and version set to 8.3"
+# Prompt user for services to install
+echo "Which services would you like to install?"
+echo "1. Git"
+echo "2. Node 20"
+echo "3. PHP 8.3"
+echo "4. MySQL"
+echo "5. Composer"
+echo "6. Vue CLI"
+echo "7. Google Cloud SDK"
+echo "8. Google SQL Proxy"
+echo "9. Nuxt3 and Yarn"
+echo "10. Setup SSH keys for git"
 
-# Add the correct path for Composer
-echo 'export PATH="/usr/local/opt/composer/bin:$PATH"' >> ~/.zshrc
-exho "Composer installed and  path added to the shell profile"
+read -p "Enter the numbers of the services you want to install (comma-separated): " services
 
-# Add the correct paths for NODEJ to the shell profile .zshrc
-echo 'export PATH="/opt/homebrew/opt/node@20/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/opt/homebrew/opt/node@20/bin:$PATH"' >> ~/.zshrc
-echo 'export LDFLAGS="-L/opt/homebrew/opt/node@20/lib" >> ~/.zshrc'
-echo 'export CPPFLAGS="-I/opt/homebrew/opt/node@20/include" >> ~/.zshrc'
-echo "NodeJS installed paths added to the shell profile"
-
-
-# Install Vue Cli
-echo "Installing Vue CLI"
-npm install -g @vue/cli
-echo "Vue CLI installed successfully!"
-
-# Install Google Cloud SDK
-echo "Installing Google Cloud SDK"
-brew install --cask google-cloud-sdk
-# Initialize Google Cloud SDK
-gcloud init
-echo "Google Cloud SDK installed successfully!"
-
-# Install Google SQL Proxy
-echo "Installing Google SQL Proxy"
-curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.10.1/cloud-sql-proxy.darwin.arm64
-chmod +x cloud_sql_proxy
-mv cloud_sql_proxy ~/cloud_sql_proxy
-
-echo "Google Cloud SQL Proxy installed successfully!"
-
-# Install Nuxt3 and Yarn Globally
-npm install -g nuxt yarn
-
-echo "Yarn Installation completed successfully!"
-
-# Create SSH keys for bitbucket
-ssh-keygen -t rsa -b 4096 -C
-
-# Add the SSH key to the ssh-agent
-eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/id_rsa
-
-# Copy the SSH key to the clipboard
-pbcopy < ~/.ssh/id_rsa.pub
-
-echo "SSH key copied to clipboard. Add it to your Bitbucket account."
-
-# Remind User to Run Mysql Secure Installation and start the mysql service
-echo "Run 'mysql_secure_installation' to secure your mysql installation"
-echo "Start the mysql service using 'brew services start mysql'"
+# Install selected services using Homebrew
+for service in $(echo $services | tr ',' ' '); do
+    case $service in
+        1)
+            echo "Installing Git"
+            brew install git
+            ;;
+        2)
+            echo "Installing Node"
+            brew install node@20
+            ;;
+        3)
+            echo "Installing PHP"
+            brew install php@8.3
+            ;;
+        4)
+            echo "Installing MySQL"
+            brew install mysql@8.3
+            ;;
+        5)
+            echo "Installing Composer"
+            brew install composer
+            ;;
+        6)
+            echo "Installing Vue CLI"
+            npm install -g @vue/cli
+            ;;
+        7)
+            echo "Installing Google Cloud SDK"
+            brew install --cask google-cloud-sdk
+            ;;
+        8)
+            echo "Installing Google SQL Proxy"
+            curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.10.1/cloud-sql-proxy.darwin.arm64
+            chmod +x cloud_sql_proxy
+            mv cloud_sql_proxy ~/cloud_sql_proxy
+            ;;
+        9)
+            echo "Installing Nuxt3 and Yarn"
+            npm install -g nuxt yarn
+            ;;
+        10)
+            echo "Creating SSH keys for Bitbucket"
+            ssh-keygen -t rsa -b 4096 -C
+            eval "$(ssh-agent -s)"
+            ssh-add -K ~/.ssh/id_rsa
+            pbcopy < ~/.ssh/id_rsa.pub
+            echo "SSH key copied to clipboard. Add it to your Bitbucket account."
+            ;;
+        *)
+            echo "Invalid service number: $service"
+            ;;
+    esac
+done
